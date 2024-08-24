f27f39668af3b37627d08874f7887fd8d977db91
ArielMariussi <arielmariussi@gmail.com>
2024-08-24 15:55:49 -0300

    Preenchendo com Inserts migration PopulaCategorias

---
+++ Migrations/20240824185102_PopulaCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UDEMY.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Model/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace UDEMY.Model
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string? ImagemUrl { get; set; }
+
+        public float Estoque { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+        public int CategoriaID { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
